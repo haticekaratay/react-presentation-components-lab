8b781af7ed990ac7f4e86692a85772d104cc5cbb
Hatice <haticekaraty@gmail.com>
2021-09-20 19:00:15 -0400

    commit tree d587c18a8547de2c3686321a82cb969b7dddeb21

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={()=> alert("Let's Dance")}/>
   </div>,
   document.getElementById('root')
 );
